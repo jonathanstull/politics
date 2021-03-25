2bd832a3e1ea5378568c8933bd6e13fd1ef160fe
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-03-25 15:35:31 -0700

    FIX: correct scripts to evaluate score
    Co-authored-by: Jeremy Banka <jeremybanka@gmail.com>
    Co-authored-by: Tiffany Greathead <tiffanygreathead@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,27 @@
 const MESSAGE_TO_CONSERVATIVE = 'You are a conservative!'
 const MESSAGE_TO_MODERATE = "You are a moderate!"
 const MESSAGE_TO_LIBERAL = "You are a liberal!"
+const MESSAGE_TO_UNDECIDED = "You are undecided!"
 
 $(document).ready(function () {
   $("#politics").submit(function () {
-    const healthValue = parseInt($("#input:radio[name=healthcare]:checked").val())
-    const nationalValue = parseInt($("#input:radio[name=national-state]:checked").val())
-    const wealthValue = parseInt($("#input:radio[name=wealth-distribution]:checked").val())
-    const score = healthValue + nationalValue + wealthValue
+    event.preventDefault();
+    const healthValue = parseInt($("input:radio[name=healthcare]:checked").val())
+    const nationalValue = parseInt($("input:radio[name=national-state]:checked").val())
+    const wealthValue = parseInt($("input:radio[name=wealth-redistribution]:checked").val())
+    const totalScore = healthValue + nationalValue + wealthValue
+    let messageToAnswerer
 
-    if (score <= 1) {
-      $("#output").text(MESSAGE_TO_CONSERVATIVE)
-    } else if (score === 2) {
-      $("#output").text(MESSAGE_TO_MODERATE)
+    if (Number.isNaN(totalScore)) {
+      messageToAnswerer = MESSAGE_TO_UNDECIDED
+    } else if (totalScore <= 1) {
+      messageToAnswer = MESSAGE_TO_CONSERVATIVE
+    } else if (totalScore === 2) {
+      messageToAnswerer = MESSAGE_TO_MODERATE
     } else {
-      $("#output").text(MESSAGE_TO_LIBERAL)
+      messageToAnswerer = MESSAGE_TO_LIBERAL
     }
+    $("#output").text(messageToAnswerer)
   })
 })
 
